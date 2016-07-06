621f26f872b8abd44c228b1b13598eece98b6595
Adam Luchini <aluchini@gmail.com>
2016-07-06 11:13:36 -0700

    corrects spelling error in folder name

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,11 +12,11 @@ namespace RPSGenerator
       Post["/"]= _ => {
         string player1 = RPSClass.GetPlayer1();
         string player2 = Request.Form["player2"];
-        RPSClass newRPSClass = new RPSClass(player1, player2);
-        return View["index.cshtml", newRPSClass.GameResult()];
+        RPSClass newRPS = new RPSClass(player1, player2);
+        return View["index.cshtml", newRPS.GameResult()];
       };
       Post["/player2"] = _ => {
-        RPSClass newRPSClass = new RPSClass(Request.Form["player1"], "");
+        RPSClass newRPS = new RPSClass(Request.Form["player1"], "");
         return View["player2.cshtml"];
       };
 
